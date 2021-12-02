2509b8b81cd093ef1ba0dd2ba4688c7eaa37aa16
John Wallace <jw388918@ohio.edu>
2021-12-02 18:42:04 -0500

    done program

--- finalquestion2.cc
+++ finalquestion2.cc
@@ -2,15 +2,34 @@
 #include <fstream>
 #include <string>
 
-void countLine(string pName){
+int countLine(string text){
+    int num = 0;
+    if (getline(text) == NULL) {
+        return num;
+    } else {
+        num = 1;
+        return num;
+    }
     return 0;
 }
 
-void countChar(string pName){
-    return 0;
+int countChar(string text){
+    int num = test.size();
+    return num;
 }
 int main(){
-    string line;
-    string char;
+    string pName;
+    string compare = "q";
+    string ohiou = "Ohio University";
+    string athens = "Athens";
+    cout << "Please enter a string. If default is wanted, enter 'q'." << endl;
+    cin >> pName;
+    if (pName == compare){
+        cout << countLine(ohiou) << " Lines." << endl;
+        cout << countChar(athens) << " Characters." << endl;
+    } else {
+        cout << countLine(pName) << " Lines." << endl;
+        cout << countChar(pName) << " Characters." << endl;
+    }
     return 0;
 }
